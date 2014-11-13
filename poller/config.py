e3cbe01b2a46aabec6f0f91b4a4c934d9e100f1c
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2014-11-13 12:44:32 +0100

    Twitter API wrapper
    * access tokens with corresponding user_id
    * methods requiring authenticated to use `fixed_access_token` field of
    RateLimitHandler; e.g. home_timeline

---
+++ poller/config.py
@@ -0,0 +1,9 @@
+# @SvvenDotCom App
+
+CONSUMER_KEY = 'Jrp1bcXiSahhWAqn3VJb4fzsg'
+CONSUMER_SECRET = '36xO8Y8YT7Y0hRHDwoULuTU2xyru6cPkCSrRxLoJAzZ3hmxhfS'
+
+ACCESS_TOKENS = {
+	2493963913: # svvendotcom
+	(u'2493963913-XBqema44oP95guiznQf1m5TGtPz7eAyEQ75wFpJ', u'TizqhmhFeFHRnRWdHfoz3OZdO5jT4DX3JiCNBqmov8muM'),
+}

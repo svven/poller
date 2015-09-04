3ba4d7b96294f801b4ccdfd21bed1f440c9d6b71
Jon Williams <jonscottw@gmail.com>
2015-09-04 21:14:16 +0100

    Fix Poller DB Errors - use the session = scoped_session from db so it is thread safe and uses connection pooling (I think!)

--- poller/twitter/job.py
+++ poller/twitter/job.py
@@ -165,6 +165,7 @@ class TimelineJob(object):
                 if sum(self.result.values()) == 0:
                     self.result = repr(e) # exception
                 logger.exception(e)
+
         finally:
             session.commit() # outside
             self.ended_at = datetime.datetime.utcnow()
